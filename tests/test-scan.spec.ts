3cfabd89da83279d6dfd2370245ded18e8ef95a9
“ozemouri” <otman@qualityaccelerators.nl>
2025-03-14 18:25:19 +0100

    Testscript update

--- tests/test-scan.spec.ts
+++ tests/test-scan.spec.ts
@@ -4,7 +4,21 @@ import AxeBuilder from '@axe-core/playwright';
 test('navigation menu flyout should not have automatically detectable accessibility violations', async ({ page }) => {
     await page.goto('https://www.qualityaccelerators.nl/');
   
-    await page.getByRole('button',{ name: 'confira aqui - Renegociação. Conte com o Itaú' }).click();
+    await page.getByText('Contact').click();
+	await page.getByRole('textbox', { name: 'Naam (verplicht)' }).click();
+	await page.getByRole('textbox', { name: 'Naam (verplicht)' }).fill('Otman');
+	await page.getByRole('textbox', { name: 'E-mailadres (verplicht)' }).click();
+	await page.getByRole('textbox', { name: 'E-mailadres (verplicht)' }).fill('otman@qualityaccelerators.nl');
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).click();
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).fill('Hoi Hoi ! Dit is een Test');
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).press('ArrowLeft');
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).press('ArrowLeft');
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).press('ArrowLeft');
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).press('ArrowRight');
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).press('ArrowRight');
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).press('ArrowRight');
+	await page.getByRole('textbox', { name: 'Bericht (verplicht)' }).fill('Hoi Hoi ! Dit is een Test!');
+	await page.getByRole('button', { name: 'Versturen' }).click();
   
     // It is important to waitFor() the page to be in the desired
     // state *before* running analyze(). Otherwise, axe might not
